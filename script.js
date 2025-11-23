f72a6fa7ed7d3fb5fea2d4d1aea816d0267b6e62
IamMasoom <133738075+IamMasoom92@users.noreply.github.com>
2025-11-23 23:50:57 +0530

    dom

---
+++ script.js
@@ -0,0 +1,29 @@
+function red() {
+  let red = document.getElementById("red");
+  red.classList.add("red");
+}
+
+
+function blue() {
+  let blue = document.getElementById("blue");
+  blue.classList.add("blue");
+}
+
+
+function green() {
+  let green = document.getElementById("green");
+  green.classList.add("green");
+}
+
+
+function yellow() {
+  let yellow = document.getElementById("yellow");
+  yellow.classList.add("yellow");
+}
+
+
+function namedisplay() {
+    const name = document.getElementById("nameinput").value;
+    const output = document.getElementById("nameoutput");
+    output.textContent = "Hello, " + name;
+}
